43c247307976b70584c4d14d12d55f03b3a916eb
John Cherney <jcherney@ford.com>
2021-12-24 23:59:13 -0500

    Add additional tests for magnitude

--- src/test/java/TestDay18.java
+++ src/test/java/TestDay18.java
@@ -12,4 +12,18 @@ public class TestDay18 {
         assertEquals(testOutput1, day18.toString());
         assertEquals(143, day18.magnitude());
     }
+
+    @Test
+    void testMagnitudes() {
+        Day18 day18 = new Day18("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]");
+        assertEquals(1384, day18.magnitude());
+        day18 = new Day18("[[[[1,1],[2,2]],[3,3]],[4,4]]");
+        assertEquals(445, day18.magnitude());
+        day18 = new Day18("[[[[3,0],[5,3]],[4,4]],[5,5]]");
+        assertEquals(791, day18.magnitude());
+        day18 = new Day18("[[[[5,0],[7,4]],[5,5]],[6,6]]");
+        assertEquals(1137, day18.magnitude());
+        day18 = new Day18("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]");
+        assertEquals(3488, day18.magnitude());
+    }
 }
