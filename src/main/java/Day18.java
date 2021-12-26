0dd3e1e96baf4d0988abb68f4b51323c50692030
John Cherney <jcherney@ford.com>
2021-12-26 00:24:51 -0500

    First pass on explode functionality. More testing needed.

--- src/main/java/Day18.java
+++ src/main/java/Day18.java
@@ -27,10 +27,10 @@ public class Day18 {
 
     public static class SNumber {
         int depth;
-        boolean isLeftGroup, isRightGroup;
-        boolean gotComma;
+        boolean isLeftGroup, isRightGroup, isSelfLeft;
+        boolean gotComma, didExplode, didSplit;
         int left, right;
-        SNumber sNumberLeft, sNumberRight;
+        SNumber sNumberLeft, sNumberRight, parent;
         public SNumber(int depth) { this.depth = depth; }
         public String toString() {
             if(isLeftGroup && isRightGroup) {
@@ -80,12 +80,15 @@ public class Day18 {
                             return sNumbers.pop();
                         } else {
                             snumber = sNumbers.pop();
+                            snumber.parent = sNumbers.peek();
                             if(sNumbers.peek().gotComma) {
                                 sNumbers.peek().sNumberRight = snumber;
                                 sNumbers.peek().isRightGroup = true;
+                                snumber.isSelfLeft = false;
                             } else {
                                 sNumbers.peek().sNumberLeft = snumber;
                                 sNumbers.peek().isLeftGroup = true;
+                                snumber.isSelfLeft = true;
                             }
                         }
                         break;
@@ -102,21 +105,152 @@ public class Day18 {
         }
         public static SNumber add(SNumber left, SNumber right) {
             SNumber n = new SNumber(0);
-            n.sNumberLeft = incrementDepth(left);
+            n.sNumberLeft = left.incrementDepth();
             n.isLeftGroup = true;
-            n.sNumberRight = incrementDepth(right);
+            n.sNumberRight = right.incrementDepth();
             n.isRightGroup = true;
-            return n;
+            left.parent = n;
+            right.parent = n;
+            left.isSelfLeft = true;
+            right.isSelfLeft = false;
+            return reduce(n);
         }
-        public static SNumber incrementDepth(SNumber n) {
-            n.depth++;
-            if(n.isLeftGroup) {
-                incrementDepth(n.sNumberLeft);
+        public static SNumber reduce(SNumber n) {
+            boolean isReduceDone = false;
+            SNumber initialValue = n, explodeValue = null, splitValue = null;
+            while(!isReduceDone) {
+                explodeValue = initialValue.explode();
+                if(initialValue.equals(explodeValue)) {
+                    splitValue = initialValue.split();
+                    if(initialValue.equals(splitValue)) {
+                        isReduceDone = true;
+                    } else {
+                        initialValue = splitValue;
+                    }
+                } else {
+                    initialValue = explodeValue;
+                }
             }
-            if(n.isRightGroup) {
-                incrementDepth(n.sNumberRight);
+            return initialValue;
+        }
+        public SNumber incrementDepth() {
+            depth++;
+            if(isLeftGroup) {
+                sNumberLeft.incrementDepth();
             }
-            return n;
+            if(isRightGroup) {
+                sNumberRight.incrementDepth();
+            }
+            return this;
+        }
+        void setDidExplodeUp(boolean value) {
+            didExplode = value;
+            if(parent != null) {
+                parent.setDidExplodeUp(value);
+            }
+        }
+        void setDidExplodeDown(boolean value) {
+            didExplode = value;
+            if(isLeftGroup) {
+                sNumberLeft.setDidExplodeDown(value);
+            }
+            if(isRightGroup) {
+                sNumberRight.setDidExplodeDown(value);
+            }
+        }
+        void setDidSplitUp(boolean value) {
+            didSplit = value;
+            if(parent != null) {
+                parent.setDidSplitUp(value);
+            }
+        }
+        void setDidSplitDown(boolean value) {
+            didSplit = value;
+            if(isLeftGroup) {
+                sNumberLeft.setDidSplitDown(value);
+            }
+            if(isRightGroup) {
+                sNumberRight.setDidSplitDown(value);
+            }
+        }
+        public SNumber explode() {
+            if(depth == 0) {
+                setDidExplodeDown(false);
+            }
+            if(depth == 3) {
+                SNumber temp = null;
+                if(isLeftGroup) {
+                    temp = sNumberLeft;
+                    sNumberLeft = null;
+                    left = 0;
+                    isLeftGroup = false;
+                    setDidExplodeUp(true);
+                    parent.handleExplodeLeftUp(temp.left);
+                    handleExplodeRightDown(temp.right);
+                } else if(isRightGroup) {
+                    temp = sNumberRight;
+                    sNumberRight = null;
+                    right = 0;
+                    isRightGroup = false;
+                    setDidExplodeUp(true);
+                    handleExplodeLeftDown(temp.left);
+                    parent.handleExplodeRightUp(temp.right);
+                }
+            } else {
+                if(!didExplode && isLeftGroup) {
+                    sNumberLeft.explode();
+                }
+                if(!didExplode && isRightGroup) {
+                    sNumberRight.explode();
+                }
+            }
+            return this;
+        }
+        public void handleExplodeLeftUp(int value) {
+            if(parent != null) {
+                if(isSelfLeft) {
+                    parent.handleExplodeLeftUp(value);
+                } else {
+                    if(isLeftGroup) {
+                        sNumberRight.handleExplodeRightDown(value);
+                    } else {
+                        left += value;
+                    }
+                }
+            }
+        }
+        public void handleExplodeLeftDown(int value) {
+            if(isLeftGroup) {
+                handleExplodeRightDown(value);
+            } else {
+                left += value;
+            }
+        }
+        public void handleExplodeRightUp(int value) {
+            if(parent != null) {
+                if(!isSelfLeft) {
+                    parent.handleExplodeRightUp(value);
+                } else {
+                    if(isRightGroup) {
+                        sNumberRight.handleExplodeLeftDown(value);
+                    } else {
+                        right += value;
+                    }
+                }
+            }
+        }
+        public void handleExplodeRightDown(int value) {
+            if(isRightGroup) {
+                handleExplodeLeftDown(value);
+            } else {
+                right += value;
+            }
+        }
+        public SNumber split() {
+            if(depth == 0) {
+                setDidSplitDown(false);
+            }
+            return this;
         }
     }
 }
--- src/test/java/TestDay18.java
+++ src/test/java/TestDay18.java
@@ -26,4 +26,33 @@ public class TestDay18 {
         day18 = new Day18("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]");
         assertEquals(3488, day18.magnitude());
     }
+    @Test
+    void testAdd1() {
+        Day18 day18 = new Day18("[1,1]\n[2,2]\n[3,3]\n[4,4]");
+        assertEquals("[[[[1,1],[2,2]],[3,3]],[4,4]]", day18.toString());
+    }
+
+    @Test
+    void testAdd2() {
+        Day18 day18 = new Day18("[1,1]\n[2,2]\n[3,3]\n[4,4]\n[5,5]");
+        assertEquals("[[[[3,0],[5,3]],[4,4]],[5,5]]", day18.toString());
+    }
+
+    @Test
+    void testAddStepByStep() {
+        String s = "[[[[4,3],4],4],[7,[[8,4],9]]] + [1,1]:\n" +
+                "\n" +
+                "after addition: [[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]\n" +
+                "after explode:  [[[[0,7],4],[7,[[8,4],9]]],[1,1]]\n" +
+                "after explode:  [[[[0,7],4],[15,[0,13]]],[1,1]]\n" +
+                "after split:    [[[[0,7],4],[[7,8],[0,13]]],[1,1]]\n" +
+                "after split:    [[[[0,7],4],[[7,8],[0,[6,7]]]],[1,1]]\n" +
+                "after explode:  [[[[0,7],4],[[7,8],[6,0]]],[8,1]]";
+        String input1 = "[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]";
+        Day18.SNumber sNumber = Day18.SNumber.factory(0, input1);
+        assertEquals("[[[[0,7],4],[7,[[8,4],9]]],[1,1]]", sNumber.explode().toString());
+        assertEquals("[[[[0,7],4],[15,[0,13]]],[1,1]]", sNumber.explode().toString());
+//        day18 = new Day18("[[[[4,3],4],4],[7,[[8,4],9]]]\n[1,1]");
+//        assertEquals("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]", day18.toString());
+    }
 }
